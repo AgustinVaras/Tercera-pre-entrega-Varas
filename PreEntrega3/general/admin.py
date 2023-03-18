7c65a12c2ba68c987b0bfe2f6125b55a86b49486
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-03-18 15:56:20 -0300

    Administracion de los modelos arreglados

--- PreEntrega3/general/admin.py
+++ PreEntrega3/general/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from general.models import Empleado
+from general.models import Empleado, Cliente, Autor, Genero, Libro 
 
 # Register your models here.
 admin.site.register(Empleado)
-# admin.site.register(Cliente)
-# admin.site.register(autores)
-# admin.site.register(Libro)
+admin.site.register(Cliente)
+admin.site.register(Autor)
+admin.site.register(Genero)
+admin.site.register(Libro)
